0613574f8c93e92807b1fddbed404e5541978097
Jesse Mugo <145917688+37863997@users.noreply.github.com>
2024-07-24 10:32:52 -0700

    blog project

---
+++ blogpr/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
